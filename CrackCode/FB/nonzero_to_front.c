b0e0ee7d71d7bf63717716e2db9a8d0004ec78b6
deepw <gemini_deepak@yahoo.com>
2017-06-16 17:04:50 -0700

    anther implementation

--- CrackCode/FB/nonzero_to_front.c
+++ CrackCode/FB/nonzero_to_front.c
@@ -19,14 +19,36 @@ move (int arr[], int n)
 			j++;
 		}
 	}
-	for (i=0; i<n; i++)
-		printf ("%d ", arr[i]);
-	printf ("\n");
 	return 0;
 }
 
+// Simpler implementation, but sure it walks the array more than once. 
+int move2 (int arr[], int n) {
+  int curr = 0;
+  for (int i=0; i < n; i++) {
+    if (arr[i] != 0) {
+      arr[curr++] = arr[i];
+    }
+  }
+  for (;curr<n; curr++) {
+    arr[curr]=0;
+  }
+  return 0;
+}
+
+void printArray(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    printf("%d\t", arr[i]);
+  }
+  printf ("\n");
+}
+
 int main () {
-	int arr[] = {1,1,1,0,0,0,9,0,9,0,9,0};
+	int arr[] = {0,2,3,0,0,0,9,0,9,0,7,0};
+        printArray(arr, sizeof(arr)/sizeof(arr[0]));
 	move (arr, sizeof(arr)/sizeof(arr[0]));
+        printArray(arr, sizeof(arr)/sizeof(arr[0]));
+	move2 (arr, sizeof(arr)/sizeof(arr[0]));
+        printArray(arr, sizeof(arr)/sizeof(arr[0]));
 	return 0;
 }
